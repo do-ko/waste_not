8cb520fd7f6f56faff0ebc62cf7b22f0a566ecdc
do-ko <doko@onet.eu>
2024-05-09 13:19:23 +0200

    reading users from firebase

--- lib/controllers/user.dart
+++ lib/controllers/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/user.dart';
 
@@ -6,4 +7,15 @@ class UserController extends ChangeNotifier {
   User? getUser(int userId) {
     return null;
   }
+
+  Future createUser({required String email, required String name}) async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc();
+    final user = User(email: email, name: name);
+    final jsonUser = user.toJson();
+    await docUser.set(jsonUser);
+  }
+
+  static Stream<List<User>> readUsers() => FirebaseFirestore.instance.collection('users')
+      .snapshots()
+      .map((snapshot) => snapshot.docs.map((doc) => User.fromMap(doc.data())).toList());
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_not/views/shared/product_list.dart';
 
+import '../controllers/user.dart';
 import '../firebase_options.dart';
 import '../models/user.dart';
 
@@ -15,7 +16,6 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   FirebaseApp? firebaseApp;
   late User? user;
-
   int productsMarked = 1;
 
   @override
@@ -57,9 +57,35 @@ class _HomeViewState extends State<HomeView> {
                     onPressed: () =>
                         Navigator.of(context).pushNamed("/product/add")),
                 IconButton(
-                    icon: const Icon(Icons.live_help_outlined),
-                    onPressed: () => {})
+                  icon: const Icon(Icons.live_help_outlined),
+                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const TestRoute())),
+                )
               ]));
         });
+  }
+}
+
+class TestRoute extends StatelessWidget {
+  const TestRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(title: const Text('User testing')),
+        body: StreamBuilder<List<User>>(
+            stream: UserController.readUsers(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('something went wrong ${snapshot.error}');
+              } else if (snapshot.hasData) {
+                final users = snapshot.data!;
+                return ListView(
+                    children: users.map((e) => Text(e.email)).toList());
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
