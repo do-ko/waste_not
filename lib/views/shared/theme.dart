b477f0181976cf5d0cf54fb9f553ed9fc084d85d
Aleksandra Broda <33639004+abroda@users.noreply.github.com>
2024-05-20 18:13:50 +0200

    Theme data

---
+++ lib/views/shared/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color backgroundHeaderColor = Color(0xffa7c8ff);
+const Color textHeaderColor = Color(0xff04305F);
+const Color iconColor = Color(0xff04305F);
+
+// TextStyles
+const TextStyle headerStyle = TextStyle(color: textHeaderColor);
