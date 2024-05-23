91c54145d21be5959f6ec29fec7d7f6565b88815
do-ko <doko@onet.eu>
2024-05-23 13:20:26 +0200

    moving the rest of product controller methods from controllers branch with adjustments

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -5,7 +5,6 @@ import 'package:waste_not/controllers/category_controller.dart';
 import 'package:waste_not/models/product.dart';
 
 class ProductController extends GetxController {
-
   RxList<ProductModel> products = RxList<ProductModel>();
 
   @override
@@ -13,7 +12,6 @@ class ProductController extends GetxController {
     super.onReady();
     fetchProductsForUser();
   }
-
 
   Future<void> fetchProductsForUser() async {
     String? currentUserId = AuthController.instance.authUser?.uid;
@@ -23,7 +21,8 @@ class ProductController extends GetxController {
       return;
     }
 
-    DocumentReference ownerRef = FirebaseFirestore.instance.doc('Users/$currentUserId');
+    DocumentReference ownerRef =
+        FirebaseFirestore.instance.doc('Users/$currentUserId');
 
     try {
       print(currentUserId);
@@ -36,11 +35,11 @@ class ProductController extends GetxController {
       } else {
         print('${snapshot.docs.length} products found.');
         List<ProductModel> productsList = snapshot.docs
-            .map((doc) => ProductModel.fromMap(doc.data() as Map<String, dynamic>))
+            .map((doc) =>
+                ProductModel.fromMap(doc.data() as Map<String, dynamic>))
             .toList();
         products.assignAll(productsList);
       }
-
     } catch (e) {
       print('Failed to fetch products: $e');
     }
@@ -57,25 +56,36 @@ class ProductController extends GetxController {
     } catch (e) {
       print('Failed to add product: $e');
     }
-    // try {
-    //   String userId = deviceStorage.read("userId") ?? "0";
-    //   DocumentReference docRef = await _firestore
-    //       .collection('users')
-    //       .doc(userId)
-    //       .collection('products')
-    //       .add(productController.product.value!.toJson());
-    //
-    //   productController.productId = docRef.id;
-    //   products.add(productController);
-    //
-    //   if (kDebugMode) {
-    //     print('Product added with ID: ${productController.productId}');
-    //   }
-    // } catch (e) {
-    //   if (kDebugMode) {
-    //     print('Error adding product: $e');
-    //   }
-    // }
+  }
+
+  Future<void> updateProduct(ProductModel product) async {
+    var collectionRef = FirebaseFirestore.instance.collection('Products');
+    try {
+      await collectionRef.doc(product.productId).update(product.toJson());
+
+      int index = products.indexWhere((p) => p.productId == product.productId);
+      if (index != -1) {
+        products[index] = product;
+      }
+    } catch (e) {
+      print('Error updating product: $e');
+    }
+  }
+
+  Future<void> removeProduct(String productId) async {
+    var collectionRef = FirebaseFirestore.instance.collection('Products');
+    try {
+      await collectionRef.doc(productId).delete();
+      products.removeWhere((product) => product.productId == productId);
+    } catch (e) {
+      print('Error removing product: $e');
+    }
+  }
+
+  Future<void> removeProducts(List<String> productIds) async {
+    for (String id in productIds) {
+      await removeProduct(id);
+    }
   }
 
 // ProductController getProduct(String productId) {
