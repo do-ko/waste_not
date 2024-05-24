53ba1bd078267edd554b327453397ba3e0fb0f8e
do-ko <doko@onet.eu>
2024-05-24 23:32:21 +0200

    suggestion wip

--- lib/views/suggestions.dart
+++ lib/views/suggestions.dart
@@ -1,27 +1,114 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:waste_not/models/product.dart';
+
+import '../controllers/model_controllers/product.dart';
+import '../controllers/model_controllers/products.dart';
 
 class SuggestionsView extends StatelessWidget {
   const SuggestionsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // UserController userController = Get.find();
-    // HomeController homeController = Get.find();
-    // ProductsController productsController = Get.find();
-    // DarkModeController darkModeController = Get.find();
+    ProductsController productsController = Get.find();
 
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(56),
-          child: AppBar(
-              elevation: 0,
-              title: const Text('Recipe suggestions'),
-              centerTitle: false),
-        ),
-        body: SingleChildScrollView(
-            child: Expanded(
-                child: Center(
-                    child: SvgPicture.asset("assets/placeholder.svg")))));
+      appBar: AppBar(title: const Text('Recipe Suggestions')),
+      body: SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Obx(
+            () => productsController.products.value.length < 3
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.kitchen, size: 100, color: Colors.grey),
+                      Text(
+                        "No products in your fridge",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          "Come back after you add some products to your fridge.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      )
+                    ],
+                  )
+                : Column(
+                    children: [
+                      const Text(
+                        'Choose three ingredients from your fridge',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 20),
+                      Obx(() => DropdownButton<String>(
+                                value: "",
+                                items: productsController.products.value
+                                    .map((productController) {
+                                  return DropdownMenuItem<String>(
+                                    value: productController.product.value?.productId,
+                                    child: Text(
+                                        productController.product.value!.name),
+                                  );
+                                }).toList(),
+                                onChanged: (String? newProduct) {
+                                  if (ProductModel != null) {
+                                    // languageController.updateLanguage(newValue);
+                                    print(newProduct);
+                                  }
+                                },
+                              )
+                          //     () => DropdownButton<String>(
+                          //   value: productsController.products,
+                          //   icon: const Icon(
+                          //     Icons.keyboard_arrow_down_sharp,
+                          //     size: 32,
+                          //   ),
+                          //   onChanged: (String? newValue) {
+                          //     if (newValue != null) {
+                          //       languageController.updateLanguage(newValue);
+                          //     }
+                          //   },
+                          //   items: languageController.languages
+                          //       .map((String value) {
+                          //     return DropdownMenuItem<String>(
+                          //       value: value,
+                          //       child: Text(
+                          //         value,
+                          //         style: const TextStyle(
+                          //           color: fontColor,
+                          //         ),
+                          //       ),
+                          //     );
+                          //   }).toList(), // Ensure this is a synchronous operation
+                          // ),
+                          ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Suggest a recipe'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.blue,
+                        ),
+                      )
+                    ],
+                  ),
+          )
+        ],
+      )),
+    );
+
+    // SingleChildScrollView(
+    //     child: Expanded(
+    //         child: Center(
+    //             child: SvgPicture.asset("assets/placeholder.svg")))));
   }
 }
